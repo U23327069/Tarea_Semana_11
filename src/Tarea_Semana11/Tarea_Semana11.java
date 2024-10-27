826d57145c8769ee414993be67d3c78646046706
miguel.coveñas <miguel.coveas@PYT08.smartglobal.pe>
2024-10-27 18:19:32 -0500

    Desarrollo de la semana 11.

--- src/Tarea_Semana11/Tarea_Semana11.java
+++ src/Tarea_Semana11/Tarea_Semana11.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Tarea_Semana11;
-
-/**
- *
- * @author miguel.coveñas
- */
+import java.util.Scanner;
 public class Tarea_Semana11 {
-    
+    public static void main(String[] args) {
+        Scanner lectura=new Scanner (System.in);
+        //Definimos la matriz de 4x4 de ventas
+        int[][] ventas = new int[4][4];
+        int totalVentas = 0;
+        //Aplicamos variables y solicitamos al usuario ingresar datos
+        System.out.println("Ingrese las ventas para la matriz 4x4:");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("Venta en la posicion [" + i + "][" + j + "]: ");
+                ventas[i][j] = lectura.nextInt();
+                totalVentas += ventas[i][j];
+            }
+        }
+        // Mostraremos la matriz de toda venta
+        System.out.println("\nMatriz de Ventas:");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(ventas[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        //Salida de datos
+        System.out.println("\nEl total de las ventas es: " + totalVentas);
+    }    
 }
